31a2fad0cbd4f5c47cdc2ffe81b23c0ee0b1307e
Shey <star131661@gmail.com>
2019-10-15 16:49:21 +0800

    加入專案檔案。

---
+++ 107327008_HW3/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace _107327008_HW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_Cx = new System.Windows.Forms.TextBox();
+            this.textBox_Cy = new System.Windows.Forms.TextBox();
+            this.textBox_Cr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_DrawCircle = new System.Windows.Forms.Button();
+            this.panel_Draw = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_XAngle = new System.Windows.Forms.TextBox();
+            this.textBox_YAngle = new System.Windows.Forms.TextBox();
+            this.textBox_ZAngle = new System.Windows.Forms.TextBox();
+            this.button_ChangeViewAngle = new System.Windows.Forms.Button();
+            this.button_PanelInitial = new System.Windows.Forms.Button();
+            this.Axis_X = new System.Windows.Forms.Label();
+            this.Axis_Y = new System.Windows.Forms.Label();
+            this.Axis_Z = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel_Draw.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_DrawCircle);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_Cr);
+            this.groupBox1.Controls.Add(this.textBox_Cy);
+            this.groupBox1.Controls.Add(this.textBox_Cx);
+            this.groupBox1.Location = new System.Drawing.Point(907, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 139);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "畫圖測試用";
+            // 
+            // textBox_Cx
+            // 
+            this.textBox_Cx.Location = new System.Drawing.Point(103, 21);
+            this.textBox_Cx.Name = "textBox_Cx";
+            this.textBox_Cx.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Cx.TabIndex = 1;
+            // 
+            // textBox_Cy
+            // 
+            this.textBox_Cy.Location = new System.Drawing.Point(103, 49);
+            this.textBox_Cy.Name = "textBox_Cy";
+            this.textBox_Cy.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Cy.TabIndex = 2;
+            // 
+            // textBox_Cr
+            // 
+            this.textBox_Cr.Location = new System.Drawing.Point(103, 77);
+            this.textBox_Cr.Name = "textBox_Cr";
+            this.textBox_Cr.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Cr.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "圓心X座標 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "圓心Y座標 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(33, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "圓半徑 : ";
+            // 
+            // button_DrawCircle
+            // 
+            this.button_DrawCircle.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_DrawCircle.Location = new System.Drawing.Point(36, 105);
+            this.button_DrawCircle.Name = "button_DrawCircle";
+            this.button_DrawCircle.Size = new System.Drawing.Size(136, 28);
+            this.button_DrawCircle.TabIndex = 1;
+            this.button_DrawCircle.Text = "繪圖";
+            this.button_DrawCircle.UseVisualStyleBackColor = true;
+            this.button_DrawCircle.Click += new System.EventHandler(this.Button_DrawCircle_Click);
+            // 
+            // panel_Draw
+            // 
+            this.panel_Draw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_Draw.Controls.Add(this.Axis_Z);
+            this.panel_Draw.Controls.Add(this.Axis_Y);
+            this.panel_Draw.Controls.Add(this.Axis_X);
+            this.panel_Draw.Location = new System.Drawing.Point(12, 12);
+            this.panel_Draw.Name = "panel_Draw";
+            this.panel_Draw.Size = new System.Drawing.Size(877, 646);
+            this.panel_Draw.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_ChangeViewAngle);
+            this.groupBox2.Controls.Add(this.textBox_ZAngle);
+            this.groupBox2.Controls.Add(this.textBox_YAngle);
+            this.groupBox2.Controls.Add(this.textBox_XAngle);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(907, 196);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(209, 214);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "旋轉視角";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(907, 445);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(209, 213);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "手臂位置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(6, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "X軸旋轉角度 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(6, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Y軸旋轉角度 : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(6, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Z軸旋轉角度 : ";
+            // 
+            // textBox_XAngle
+            // 
+            this.textBox_XAngle.Location = new System.Drawing.Point(115, 18);
+            this.textBox_XAngle.Name = "textBox_XAngle";
+            this.textBox_XAngle.Size = new System.Drawing.Size(88, 22);
+            this.textBox_XAngle.TabIndex = 6;
+            // 
+            // textBox_YAngle
+            // 
+            this.textBox_YAngle.Location = new System.Drawing.Point(115, 67);
+            this.textBox_YAngle.Name = "textBox_YAngle";
+            this.textBox_YAngle.Size = new System.Drawing.Size(88, 22);
+            this.textBox_YAngle.TabIndex = 9;
+            // 
+            // textBox_ZAngle
+            // 
+            this.textBox_ZAngle.Location = new System.Drawing.Point(115, 117);
+            this.textBox_ZAngle.Name = "textBox_ZAngle";
+            this.textBox_ZAngle.Size = new System.Drawing.Size(88, 22);
+            this.textBox_ZAngle.TabIndex = 10;
+            // 
+            // button_ChangeViewAngle
+            // 
+            this.button_ChangeViewAngle.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_ChangeViewAngle.Location = new System.Drawing.Point(36, 177);
+            this.button_ChangeViewAngle.Name = "button_ChangeViewAngle";
+            this.button_ChangeViewAngle.Size = new System.Drawing.Size(136, 31);
+            this.button_ChangeViewAngle.TabIndex = 6;
+            this.button_ChangeViewAngle.Text = "旋轉";
+            this.button_ChangeViewAngle.UseVisualStyleBackColor = true;
+            this.button_ChangeViewAngle.Click += new System.EventHandler(this.Button_ChangeViewAngle_Click);
+            // 
+            // button_PanelInitial
+            // 
+            this.button_PanelInitial.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_PanelInitial.Location = new System.Drawing.Point(916, 12);
+            this.button_PanelInitial.Name = "button_PanelInitial";
+            this.button_PanelInitial.Size = new System.Drawing.Size(194, 33);
+            this.button_PanelInitial.TabIndex = 0;
+            this.button_PanelInitial.Text = "畫布初始化";
+            this.button_PanelInitial.UseVisualStyleBackColor = true;
+            this.button_PanelInitial.Click += new System.EventHandler(this.Button_PanelInitial_Click);
+            // 
+            // Axis_X
+            // 
+            this.Axis_X.AutoSize = true;
+            this.Axis_X.Location = new System.Drawing.Point(310, 264);
+            this.Axis_X.Name = "Axis_X";
+            this.Axis_X.Size = new System.Drawing.Size(13, 12);
+            this.Axis_X.TabIndex = 0;
+            this.Axis_X.Text = "X";
+            // 
+            // Axis_Y
+            // 
+            this.Axis_Y.AutoSize = true;
+            this.Axis_Y.Location = new System.Drawing.Point(187, 295);
+            this.Axis_Y.Name = "Axis_Y";
+            this.Axis_Y.Size = new System.Drawing.Size(13, 12);
+            this.Axis_Y.TabIndex = 1;
+            this.Axis_Y.Text = "Y";
+            // 
+            // Axis_Z
+            // 
+            this.Axis_Z.AutoSize = true;
+            this.Axis_Z.Location = new System.Drawing.Point(236, 183);
+            this.Axis_Z.Name = "Axis_Z";
+            this.Axis_Z.Size = new System.Drawing.Size(12, 12);
+            this.Axis_Z.TabIndex = 2;
+            this.Axis_Z.Text = "Z";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 670);
+            this.Controls.Add(this.button_PanelInitial);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel_Draw);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel_Draw.ResumeLayout(false);
+            this.panel_Draw.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_Cr;
+        private System.Windows.Forms.TextBox textBox_Cy;
+        private System.Windows.Forms.TextBox textBox_Cx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_DrawCircle;
+        private System.Windows.Forms.Panel panel_Draw;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_ChangeViewAngle;
+        private System.Windows.Forms.TextBox textBox_ZAngle;
+        private System.Windows.Forms.TextBox textBox_YAngle;
+        private System.Windows.Forms.TextBox textBox_XAngle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button_PanelInitial;
+        private System.Windows.Forms.Label Axis_Z;
+        private System.Windows.Forms.Label Axis_Y;
+        private System.Windows.Forms.Label Axis_X;
+    }
+}
+
---
+++ 107327008_HW3/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _107327008_HW3
+{
+    public partial class Form1 : Form
+    {
+        Pen penRed, penBlue, penGreen, penGray, penMag;
+        Graphics myGraph;
+        Rectangle rectCir = new Rectangle(0, 0, 100, 200);
+        Point CenterPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            penRed = new Pen(Color.Red, 2);
+            penGreen = new Pen(Color.Green, 2);
+            penBlue = new Pen(Color.Blue, 2);
+            penGray = new Pen(Color.Gray, 1);
+            penMag = new Pen(Color.Magenta);
+            myGraph = this.panel_Draw.CreateGraphics();
+            CenterPoint = new Point(panel_Draw.Location.X + (panel_Draw.Width / 2), panel_Draw.Location.Y + (panel_Draw.Height / 2));
+            Axis_X.Visible = Axis_Y.Visible = Axis_Z.Visible = false;                               //關閉坐標軸標示
+        }
+        private void Button_PanelInitial_Click(object sender, EventArgs e)
+        {
+            panel_draw_Paint(sender, null);                                         //讓畫布觸發
+            drawLine(penBlue, 0, 0, 100, 0);                                        //藍色的X軸
+            drawLine(penGreen, 0, 0, 0, 100);                                       //綠色的Y軸
+            drawCircle(penRed, 0, 0, 5);                                            //紅色的Z軸
+            Axis_X.Visible = Axis_Y.Visible = true;                                 //開啟坐標軸標示
+            Axis_X.Location = new Point(CenterPoint.X + 120, CenterPoint.Y - 10);   //給定標示座標
+            Axis_Y.Location = new Point(CenterPoint.X, CenterPoint.Y - 120);
+
+            textBox_Cx.Text = textBox_Cy.Text = textBox_Cr.Text = "0";
+            textBox_XAngle.Text = textBox_YAngle.Text = textBox_ZAngle.Text = "0";            
+        }
+        private void Button_ChangeViewAngle_Click(object sender, EventArgs e)
+        {
+            
+            panel_draw_Paint(sender, null);                                         //讓畫布觸發
+            double deltXAng, deltYAng, deltZAng;
+            double Deg2Rad = Math.PI / 180.0;
+
+            deltXAng = Convert.ToDouble(textBox_XAngle.Text) * Deg2Rad;
+            deltYAng = Convert.ToDouble(textBox_YAngle.Text) * Deg2Rad;
+            deltZAng = Convert.ToDouble(textBox_ZAngle.Text) * Deg2Rad;
+            double[][] MatrixAngX = new double[][]
+            {
+                new double[] { 1, 0, 0 },
+                new double[] { 0, Math.Cos(deltXAng), Math.Sin(deltXAng) },
+                new double[] { 0, -Math.Sin(deltXAng), Math.Cos(deltXAng) }
+            };   //宣告X軸的轉換矩陣
+            double[][] MatrixAngY = new double[][]
+            { 
+                new double[] { Math.Cos(deltYAng), 0, -Math.Sin(deltYAng) }, 
+                new double[] { 0, 1, 0 }, 
+                new double[] { Math.Sin(deltYAng), 0, Math.Cos(deltYAng) }
+            };  //宣告Y軸的轉換矩陣
+            double[][] MatrixAngZ = new double[][] 
+            { 
+                new double[] { Math.Cos(deltZAng), Math.Sin(deltZAng), 0 },
+                new double[] { -Math.Sin(deltZAng), Math.Cos(deltZAng), 0 },
+                new double[] { 0, 0, 1 }
+            };  //宣告Z軸的轉換矩陣
+            double[][] MatrixAng = MatrixMult(MatrixAngX,MatrixAngY);
+            MatrixAng = MatrixMult(MatrixAng,MatrixAngZ);                   //把三個轉換矩陣乘起來
+            double[] viewVectorX = transView(MatrixAng, 100, 0, 0);          //轉換X軸
+            drawLine(penBlue, 0, 0, viewVectorX[0], viewVectorX[1]);
+            double[] viewVectorY = transView(MatrixAng, 0, 100, 0);          //轉換Y軸
+            drawLine(penGreen, 0, 0, viewVectorY[0], viewVectorY[1]);
+            double[] viewVectorZ = transView(MatrixAng, 0, 0, 100);          //轉換Z軸
+            drawLine(penRed, 0, 0, viewVectorZ[0], viewVectorZ[1]);
+
+        }
+
+        private void Button_DrawCircle_Click(object sender, EventArgs e)
+        {
+            panel_draw_Paint(sender, null);                                         //讓畫布觸發
+            drawLine(penBlue, 0, 0, 100, 0);                                        //藍色的X軸
+            drawLine(penGreen, 0, 0, 0, 100);                                       //綠色的Y軸
+            Axis_X.Visible = Axis_Y.Visible = true;                                 //開啟坐標軸標示
+            Axis_X.Location = new Point(CenterPoint.X + 120, CenterPoint.Y - 10);   //給定標示座標
+            Axis_Y.Location = new Point(CenterPoint.X, CenterPoint.Y - 120);
+            try
+            {
+                double centerX = Convert.ToDouble(textBox_Cx.Text);
+                double centerY = Convert.ToDouble(textBox_Cy.Text);
+                double rad = Convert.ToDouble(textBox_Cr.Text);
+                drawCircle(penRed, centerX, centerY, rad);
+                drawLine(penBlue, centerX, centerY, 0, 0);
+            }
+            catch
+            {
+                MessageBox.Show("數值輸入錯誤", "Error ! !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+            
+        }
+        public void drawCircle(Pen penX, double centerX, double centerY, double rad)
+        {
+            int cx = (int)centerX + CenterPoint.X;
+            int cy = CenterPoint.Y - (int)centerY;
+            int radInt = (int)rad;
+            rectCir.X = cx - radInt;
+            rectCir.Y = cy - radInt;
+            rectCir.Width = 2 * radInt;
+            rectCir.Height = 2 * radInt;
+            myGraph.DrawArc(penX, rectCir, 0, 360);
+            return;
+        }
+        public void drawLine(Pen penX, double x1, double y1, double x2, double y2)
+        {
+            int cx = (int)x1 + CenterPoint.X;
+            int cy = CenterPoint.Y - (int)y1;
+            int dx = (int)x2 + CenterPoint.X;
+            int dy = CenterPoint.Y - (int)y2;
+            myGraph.DrawLine(penX, cx, cy, dx, dy);
+            return;
+        }
+        private void panel_draw_Paint(object sender, PaintEventArgs e)
+        {
+            myGraph.Clear(this.BackColor);
+            myGraph.DrawLine(penGray, CenterPoint.X, 0, CenterPoint.X, panel_Draw.Height);
+            myGraph.DrawLine(penGray, 0, CenterPoint.Y, panel_Draw.Width, CenterPoint.Y);
+        }
+        private double[][] MatrixMult(double[][] matrix1, double[][] matrix2)
+        {
+            int m = matrix1.Length, n = matrix2.Length, p = matrix2[0].Length;
+            double[][] result = new double[m][];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = new double[p];
+            }            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        result[i][j] += (matrix1[i][k] * matrix2[k][j]);
+                    }
+                }
+            }
+            return result;
+        }  
+        public double[] transView(double[][] matA, double x, double y, double z)
+        {
+            double[] VectorOut = new double[2];
+            double[] VectorNew = new double[3] { 0, 0, 0 };
+            double[] Vector = new double[3] { x, y, z };
+            for (int i = 0; i < VectorNew.Length; i++)
+            {
+                for (int j = 0; j < VectorNew.Length; j++)
+                {
+                    VectorNew[i] += matA[i][j] * Vector[j];
+                }
+            }
+            VectorOut[0] = VectorNew[0];
+            VectorOut[1] = VectorNew[1];
+            return VectorOut;
+        }
+    }
+}
